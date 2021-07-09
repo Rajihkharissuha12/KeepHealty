9a322fc9dd28eef512c15c72bc4482ce4528fb29
RifqiRE17 <72175584+RifqiRE17@users.noreply.github.com>
2021-07-09 20:56:00 +0700

    pembaruhan

--- app/src/main/java/com/example/keephealty/api/ApiClient.java
+++ app/src/main/java/com/example/keephealty/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.43.200/applogindanregisterandroid/";
+    private static final String BASE_URL = "http://192.168.43.135/applogindanregisterandroid/";
 
     private static Retrofit retrofit;
 
