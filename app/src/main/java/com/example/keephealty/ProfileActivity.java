b52f2f62854e0a241a48a5b174f496df0a8460e2
FigoFajarYulianto <figofajar07@gmail.com>
2021-07-09 16:47:34 +0700

    pembaruan

--- app/src/main/java/com/example/keephealty/ProfileActivity.java
+++ app/src/main/java/com/example/keephealty/ProfileActivity.java
@@ -1,2 +1,14 @@
 package com.example.keephealty;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
